c06f673a7f528f5c3c3ea9b5196c25f6eae3aa05
Jens Gerntholtz <jens@io.co.za>
2019-05-20 08:51:02 +0200

    first working iteration of the node server

--- mysample.js
+++ mysample.js
@@ -1,34 +1,62 @@
-http = require("http");
-fs = require("fs");
+http = require('http');
+fs = require('fs');
 
 port = 3000;
-host = "127.0.0.1";
+host = '127.0.0.1';
+
+// serial port initialization:
+var SerialPort = require('serialport'); // include the serialport library
+var portName = process.argv[2]; // get the port name from the command line
+var myPort = new SerialPort(portName, 9600); // open the port
+
+// these are the definitions for the serial events:
+myPort.on('open', openPort); // called when the serial port opens
+
+function openPort() {
+  var brightness = 1; // the brightness to send for the LED
+  console.log('port open');
+  console.log('baud rate: ' + myPort);
+
+  // since you only send data when the port is open, this function
+  // is local to the openPort() function:
+}
+
+function sendData(roundKills) {
+  // convert the value to an ASCII string before sending it:
+  myPort.write(roundKills.toString());
+  console.log('Sending ' + roundKills + ' out the serial port');
+}
 
 server = http.createServer(function(req, res) {
-  if (req.method == "POST") {
-    console.log("Handling POST request...");
-    res.writeHead(200, { "Content-Type": "text/html" });
+  if (req.method == 'POST') {
+    console.log('Handling POST request...');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
 
-    var body = "";
-    req.on("data", function(data) {
-      body += data;
+    var body = [];
+    req.on('data', chunk => {
+      body.push(chunk);
     });
-    req.on("end", function() {
-      console.log("POST payload: " + body);
-      res.end("");
+    req.on('end', function() {
+      // player/state/round_kills
+      // console.log(body.player.state.round_kills);
+      console.log(JSON.parse(body));
+      console.log(JSON.parse(body).player.state.round_kills);
+      sendData(JSON.parse(body).player.state.round_kills);
+
+      res.end('');
     });
   } else {
-    console.log("Not expecting other request types...");
-    res.writeHead(200, { "Content-Type": "text/html" });
+    console.log('Not expecting other request types...');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     var html =
-      "<html><body>HTTP Server at http://" +
+      '<html><body>HTTP Server at http://' +
       host +
-      ":" +
+      ':' +
       port +
-      "</body></html>";
+      '</body></html>';
     res.end(html);
   }
 });
 
 server.listen(port, host);
-console.log("Listening at http://" + host + ":" + port);
+console.log('Listening at http://' + host + ':' + port);
